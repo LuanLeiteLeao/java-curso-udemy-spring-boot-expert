882b44b06643bdc2b8ff0e9306f5e7e588da9cc1
Luan Leite Leão <luan.leite@squadra.com.br>
2022-07-06 17:08:47 -0300

    72. Configurando as Roles e Authorities

--- src/main/java/io/github/luanleiteleao/config/SecurityConfig.java
+++ src/main/java/io/github/luanleiteleao/config/SecurityConfig.java
@@ -1,9 +1,44 @@
 package io.github.luanleiteleao.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();        
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // altentica: altentica um usuário
+        auth
+        .inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .withUser("user")
+        .password(passwordEncoder().encode("user"))
+        .roles("USER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // altorização: valida se um usuário tem autorização para acessar um determinado path
+        http
+        .csrf().disable()
+        .authorizeRequests()
+            .antMatchers("/api/clientes/**")
+                .authenticated()
+            .and()
+                .formLogin();
+    }
 
 }

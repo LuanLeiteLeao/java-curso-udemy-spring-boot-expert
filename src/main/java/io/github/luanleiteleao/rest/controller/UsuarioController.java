45d3e80104d8b37c3b7b276a52e5f8fb2cee626d
LuanLeiteLeao <luanleiteraio@gmail.com>
2022-07-07 18:15:22 -0300

    Aula 83. Testando o token JWT no Postmanusuarios

--- src/main/java/io/github/luanleiteleao/rest/controller/UsuarioController.java
+++ src/main/java/io/github/luanleiteleao/rest/controller/UsuarioController.java
@@ -1,30 +1,62 @@
 package io.github.luanleiteleao.rest.controller;
 
 import io.github.luanleiteleao.domain.entity.Usuario;
-import io.github.luanleiteleao.service.imp.UsuarioServiceImp;
+import io.github.luanleiteleao.exception.SenhaInvalidaException;
+import io.github.luanleiteleao.rest.dto.CredenciaisDTO;
+import io.github.luanleiteleao.rest.dto.TokenDTO;
+import io.github.luanleiteleao.security.jwt.JwtService;
+import io.github.luanleiteleao.service.imp.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
-    @Autowired
-    private UsuarioServiceImp service;
 
-    @Autowired
-    private PasswordEncoder encoder;
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Usuario salvar(@RequestBody @Valid Usuario usuario){
-        String senhaCriptografada = encoder.encode(usuario.getSenha());
+    public Usuario salvar( @RequestBody @Valid Usuario usuario ){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
         usuario.setSenha(senhaCriptografada);
-        return service.salvar(usuario);
+        return usuarioService.salvar(usuario);
     }
+
+    @PostMapping("/auth")
+    public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais){
+        try{
+            Usuario usuario = Usuario.builder()
+                    .login(credenciais.getLogin())
+                    .senha(credenciais.getSenha()).build();
+            UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+            String token = jwtService.gerarToken(usuario);
+            return new TokenDTO(usuario.getLogin(), token);
+        } catch (UsernameNotFoundException | SenhaInvalidaException e ){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
+
 }
